0165bf58f681ab65c6c2fc7b10408213222ef8d1
Panav Munshi <panav.munshi@gmail.com>
2020-06-10 17:28:14 -0700

    Changelist: 1) PolylineSliderGraph: Introducing PolylineSliderGraph. It is basically a refactor of the original code from PolylineSlider.kt
    2) PolylineSlider: Rewrite class as a Relative Layout that houses multiple views, such as the PolylineSliderGraph class.
    
    Signed-off-by: Panav Munshi <panav.munshi@gmail.com>

--- PolylineSlider/src/main/java/olil3/polylineSlider/PolylineSlider.kt
+++ PolylineSlider/src/main/java/olil3/polylineSlider/PolylineSlider.kt
@@ -1,47 +1,26 @@
 package olil3.polylineSlider
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Color
 import android.util.AttributeSet
 import android.util.Log
-import android.view.View
-import android.view.ViewGroup
-import android.widget.HorizontalScrollView
 import android.widget.RelativeLayout
-import android.widget.SeekBar
-import com.h6ah4i.android.widget.verticalseekbar.VerticalSeekBar
-import com.h6ah4i.android.widget.verticalseekbar.VerticalSeekBarWrapper
-import olil3.polylineSlider.utils.EPointF
-import olil3.polylineSlider.utils.PolyBezierPathUtil
-import kotlin.math.abs
+import olil3.polylineSlider.utils.PolylineSliderGraph
 
 class PolylineSlider : RelativeLayout {
-    private lateinit var mSliderWrapperViewIDs: IntArray
-    private var mNumberOfDataPoints: Int = 0
-    private var mThumbColor: Int = 0
-    private lateinit var mSliderThumbColor: PorterDuffColorFilter
-    private var sliderAlphaValue: Int? = 0
-    private val mThumbCoordinateList: HashMap<Int, EPointF> = hashMapOf()
-    private var ySliderThumbPos: Float = 0.0f
-    private val bezierPathPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var isBaseUIInitialized = false
-    private var viewWidth = 0
-    private var viewHeight = 0
+    private var mPolylineSliderGraph: PolylineSliderGraph
 
     constructor(mContext: Context, attributeSet: AttributeSet?, defStyleAttr: Int) : super(
         mContext,
         attributeSet,
         defStyleAttr
     ) {
+        /* As this layout acts as a housing for multiple subviews, disable drawing to avoid misuse of resources. */
         setWillNotDraw(false)
-        objectInit(mContext, attributeSet)
-    }
 
-    constructor(mContext: Context, attributeSet: AttributeSet?) : this(mContext, attributeSet, 0)
-    constructor(mContext: Context) : this(mContext, null, 0)
-
-    private fun objectInit(mContext: Context, attributeSet: AttributeSet?) {
-        View.inflate(mContext, R.layout.polyline_slider, this)
+        var mNumberOfDataPoints = 2
+        var sliderAlphaValue: Int? = 0
+        var mThumbColor: Int = Color.MAGENTA
 
         if (attributeSet != null) {
             val attributes = mContext.obtainStyledAttributes(
@@ -52,7 +31,7 @@ class PolylineSlider : RelativeLayout {
             )
             try {
                 mNumberOfDataPoints =
-                    attributes.getInt(R.styleable.PolylineSlider_number_of_data_points, 1)
+                    attributes.getInt(R.styleable.PolylineSlider_number_of_data_points, 2)
                 sliderAlphaValue =
                     attributes.getInt(R.styleable.PolylineSlider_is_slider_track_visible, 0)
                 mThumbColor =
@@ -68,115 +47,17 @@ class PolylineSlider : RelativeLayout {
                 throw IllegalArgumentException(mContext.resources.getString(R.string.invalid_number_of_data_points))
             }
 
-            bezierPathPaint.color = mThumbColor
-            bezierPathPaint.style = Paint.Style.STROKE
-            bezierPathPaint.strokeWidth = 5f
-
-            mSliderThumbColor = PorterDuffColorFilter(Color.MAGENTA, PorterDuff.Mode.SRC_ATOP)
-            mSliderWrapperViewIDs = IntArray(mNumberOfDataPoints)
         }
+
+        mPolylineSliderGraph = PolylineSliderGraph(
+            mNumberOfDataPoints,
+            sliderAlphaValue,
+            mThumbColor,
+            mContext
+        )
+        this.addView(mPolylineSliderGraph)
     }
 
-    private fun initializeBaseUI() {
-        val mScrollViewRelativeLayout: RelativeLayout =
-            findViewById<HorizontalScrollView>(R.id.polylineHorizontalScrollView).findViewById(R.id.polylineRelativeLayout)
-
-        for (sliderWrapperPos in 0 until mNumberOfDataPoints) {
-            val mSlider = VerticalSeekBar(context)
-            val mSliderWrapper = VerticalSeekBarWrapper(context)
-
-            mSliderWrapper.id = View.generateViewId()
-            mSliderWrapperViewIDs[sliderWrapperPos] = mSliderWrapper.id
-
-            mSlider.rotationAngle = VerticalSeekBar.ROTATION_ANGLE_CW_270
-            mSlider.max = 100
-            mSlider.progress = 50
-            mSlider.splitTrack = false
-            mSlider.progressDrawable.alpha = sliderAlphaValue!!
-            mSlider.thumb.colorFilter = mSliderThumbColor
-            mSlider.progressDrawable.colorFilter = mSliderThumbColor
-
-            mSlider.post {
-                mThumbCoordinateList[mSliderWrapper.id] = getThumbXYCoordinatesAsEPointF(mSlider)
-            }
-
-            mSlider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(
-                    seekBar: SeekBar?,
-                    progress: Int,
-                    fromUser: Boolean
-                ) {
-                    mThumbCoordinateList[mSliderWrapper.id] =
-                        getThumbXYCoordinatesAsEPointF(mSlider)
-                    invalidate()
-                }
-
-                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-
-            })
-
-            mSliderWrapper.addView(mSlider)
-
-            val sliderPositioningParams =
-                LayoutParams(150, ViewGroup.LayoutParams.MATCH_PARENT)
-            if (sliderWrapperPos != 0) {
-                sliderPositioningParams.addRule(
-                    RIGHT_OF,
-                    mSliderWrapperViewIDs[sliderWrapperPos - 1]
-                )
-                mScrollViewRelativeLayout.addView(mSliderWrapper, sliderPositioningParams)
-
-            } else {
-                mScrollViewRelativeLayout.addView(mSliderWrapper, sliderPositioningParams)
-                mSliderWrapper.post {
-                    ySliderThumbPos = getThumbXYCoordinatesAsEPointF(mSlider).y // Get Y - element
-                }
-            }
-        }
-    }
-
-    private fun getThumbXYCoordinatesAsEPointF(seekBarToFind: VerticalSeekBar): EPointF {
-        val seekBarWrapper = seekBarToFind.parent as VerticalSeekBarWrapper
-        val seekBarThumbBounds = seekBarToFind.thumb.bounds
-        val xPos: Float =
-            seekBarWrapper.left + seekBarThumbBounds.exactCenterY() + ((seekBarWrapper.width - (seekBarToFind.paddingLeft * 1.1f)) / 2)
-        val yPos: Float =
-            seekBarWrapper.bottom - seekBarThumbBounds.exactCenterX() - (seekBarThumbBounds.height() * 0.4f)
-
-        return EPointF(xPos, yPos)
-    }
-
-    private fun getBezierPathForThumbs(): Path? {
-        return if (mNumberOfDataPoints == mThumbCoordinateList.entries.size) {
-            val mListOfEPointFs = arrayListOf<EPointF>()
-
-            for (i in 0 until mNumberOfDataPoints) {
-                mListOfEPointFs.add(mThumbCoordinateList[mSliderWrapperViewIDs[i]]!!)
-            }
-            PolyBezierPathUtil().computePathThroughKnots(mListOfEPointFs)
-        } else {
-            null
-        }
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        val pathToDraw = getBezierPathForThumbs()
-        if (pathToDraw != null) {
-            canvas?.drawPath(pathToDraw, bezierPathPaint)
-        }
-        Log.e("Canvas", "Executing")
-    }
-
-    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        super.onLayout(changed, l, t, r, b)
-        if (!isBaseUIInitialized) {
-            viewHeight = abs(t - b)
-            viewWidth = abs(r - l)
-            initializeBaseUI()
-            isBaseUIInitialized = true
-            invalidate()
-        }
-    }
+    constructor(mContext: Context, attributeSet: AttributeSet?) : this(mContext, attributeSet, 0)
+    constructor(mContext: Context) : this(mContext, null, 0)
 }
