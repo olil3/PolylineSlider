922c3619b9aac5036ba4e334e1b0c9391e4a75c4
Panav Munshi <panav.munshi@gmail.com>
2020-08-05 23:17:05 -0700

    Update debugging code to account for getSliderProgresAsXXXXXX() functions
    Signed-off-by: Panav Munshi <panav.munshi@gmail.com>

--- app/src/main/java/olil3/polyLineSlider/MainActivity.kt
+++ app/src/main/java/olil3/polyLineSlider/MainActivity.kt
@@ -1,11 +1,28 @@
 package olil3.polyLineSlider
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import olil3.polylineSlider.PolylineSlider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mPolylineSlider = findViewById<PolylineSlider>(R.id.mPolylineSlider)
+
+        findViewById<Button>(R.id.getPercentageMap).setOnClickListener {
+            Toast.makeText(
+                this,
+                "${mPolylineSlider.getSliderProgressAsPercentage()}",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        findViewById<Button>(R.id.getValueMap).setOnClickListener {
+            Toast.makeText(this, "${mPolylineSlider.getSliderProgressAsValue()}", Toast.LENGTH_LONG)
+                .show()
+        }
     }
 }
